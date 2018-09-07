d11a3932469d44eed2dd9591a73668e9aaeb45f5
Marko Samoilovic <msamoilovic@gmail.com>
2018-09-07 11:33:59 +0200

    Dodata ruta za kreiranje postova

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+const Post = require('../../models/Post')
+const validatePostInput = require('../../validation/posts')
 
 //@route GET api/posts/test
 //@desc Testira post rutu
 //@access Public
 router.get('/test', (req, res) => {
     res.json({msg: "Posts works"})
+})
+
+//@route POST api/posts/
+//@desc Kreira post
+//@access Private
+
+router.post('/', passport.authenticate('jwt', {session: false}),(req, res) => {
+    const { errors, isValid} = validatePostInput(req.body)
+    
+    if(!isValid) {
+      return res.status(400).json(errors)
+    }
+
+    const newPost = new Post ({
+        text: req.body.text,
+        name: req.body.name,
+        user: req.user.id
+    });
+
+    newPost.save().then( post => res.json(post))
 })
 
 module.exports = router
