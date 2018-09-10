3eaf728949030c8d76f9a184f3bbcdbd423d68ce
Marko Samoilovic <msamoilovic@gmail.com>
2018-09-10 16:10:20 +0200

    Dodati Socketi

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
 
 const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(cors())
 
 // Passport
 app.use(passport.initialize())
@@ -34,8 +40,12 @@ app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
 
-
-
 const port =  process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+//Dodavanje Socketa
+io.on('connection', (socket)=> {
+  
+})
+
+
+server.listen(port, () => console.log(`Server running on port ${port}`))
