e71079c45a1f5b8f84c2d05af8e797d211a9ac85
Marko Samoilovic <msamoilovic@gmail.com>
2018-09-11 11:18:25 +0200

    Dodate rute za postove

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 
 const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
 const validatePostInput = require('../../validation/posts')
 
 //@route GET api/posts/test
@@ -31,6 +32,46 @@ router.post('/', passport.authenticate('jwt', {session: false}),(req, res) => {
     });
 
     newPost.save().then( post => res.json(post))
+})
+
+//@route GET api/posts
+//@desc Dobavlja sve postove
+//@access Pubic
+
+router.get('/' , (req, res) => {
+    Post.find()
+     .sort({date: -1})
+     .then(posts => res.json(posts))
+     .catch(err => res.status(404).json({nopostfound : 'No posts found'}))
+})
+
+//@route GET api/posts/:id
+//@desc Dobavlja post sa odredjenim ID
+//@access Public
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+     .then(post => res.json(post))
+     .catch(err => res.status(404).json({nopostfound : 'No post found'}))
+})
+
+//@route DELETE api/posts/:id
+//@desc Brise post sa odredjenim ID
+//@access Private
+
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Profile.findOne({user: req.user.id})
+     .then(profile => {
+        Post.findById(req.params.id)
+         .then(post => {
+            if(post.user.toString() !== req.user.id) {
+              return res.status(401).json({authorized: 'Not Authorized'})
+            }
+         })
+         .catch(err => res.json(err))
+     })
+     .catch(err => res.status(404).json({user: 'User not found'}))
+
 })
 
 module.exports = router
