ec7361e1e0adfad2c97b8e8c9d4bcae3b937a149
Marko Samoilovic <msamoilovic@gmail.com>
2018-09-19 11:42:34 +0200

    Dodate rute za like i unlike postova

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -67,11 +67,60 @@ router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res)
             if(post.user.toString() !== req.user.id) {
               return res.status(401).json({authorized: 'Not Authorized'})
             }
+
+            post.remove()
+             .then( res => res.json({success: 'true'}))
          })
          .catch(err => res.json(err))
      })
      .catch(err => res.status(404).json({user: 'User not found'}))
 
+})
+
+//@route POST api/posts/like/:id
+//@desc Dodaje like funkcionalnost
+//@access Private
+
+router.post('/like/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id})
+   .then(profile => {
+    Post.findById(req.params.id)
+     .then(post => {
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+          return res.status(400).json({alreadyliked: 'You already liked this post'})    
+        }
+
+        post.likes.push({user: req.user.id});
+
+        post.save().then(post => res.json(post))
+     })
+     .catch(err => res.json(err)) 
+   })
+   .catch(err => res.json(err))
+})
+
+//@route POST api/posts/unlike/:id
+//@desc Unlike za post
+//@access Private
+
+router.post('/unlike/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Profile.findOne({user: req.user.id})
+     .then(profile => {
+        Post.findById(req.params.id)
+         .then(post => {
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length == 0) {
+                return res.status(400).json({unliked: 'You have not liked post'})
+            }
+
+            const removeIndex = post.likes.map(item => item.user.toString()).indexOf(req.user.id)
+
+            post.likes.splice(removeIndex, 1)
+
+            post.save().then(post => res.json(post))
+         })
+         .catch(err => res.json(err))
+     })
+     .catch(err => res.json(err))
 })
 
 module.exports = router
